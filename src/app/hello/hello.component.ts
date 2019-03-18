1f47a93134f3691e4784046710561da1bf654e29
mikef1234 <mikejferrari12@gmail.com>
2019-03-17 20:02:52 -0400

    des

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HelloComponent implements OnInit {
 
+myvariable = "Mike"
+bitwiseOR = 2 | 5 //10 101
+i = 0
+someVar = Math.random()
   constructor() { }
+
+  doSomeHeavyTask(){
+  	console.log(`Called ${this.i++} times`)
+  }
 //lifecycle hook
   ngOnInit() {
+  	/*setInterval(()=>{
+  		this.someVar = Math.random()
+  	},50)*/
   }
 
 }
