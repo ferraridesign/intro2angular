e5ab173449ddf233218c7b1e9ca9b0169b76879f
ferraridesign <mferraridesign@gmail.com>
2019-03-20 09:58:43 -0400

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import {HelloComponent} from './hello/hello.component'
 import {RecordsService} from './records.service';
+
+interface myData {
+  obj: Object
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
