e7f27d9baea16c9654b74fc555ce7dba36eb0586
ferraridesign <mferraridesign@gmail.com>
2019-03-20 10:13:24 -0400

    Update records.service.ts

--- src/app/records.service.ts
+++ src/app/records.service.ts
@@ -13,7 +13,7 @@ export class RecordsService {
 //rxjs asynchronos pipe where your endpoint is where u subscribe to
   //php -$ localhost:1234 to run a local environment
 getData(){
-		return this.http.get<myData>('http://localhost:1234/file.php')
+		return this.http.get<myData>('/api/file.php')
    
 }
 
